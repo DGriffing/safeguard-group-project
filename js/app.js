15b3c57dae6c11294759a4f170fd17fa6a499a76
adamsebetich@yahoo.com <adamsebetich@yahoo.com>
2015-12-02 14:46:09 -0800

    template

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
 'use strict';
+
+angular.module('SafeguardApp', ['ngSanitize', 'ui.router', 'ui.bootstrap'])
+
+.config(function($stateProvider){
+
+	$stateProvider
+		.state('home', {
+			url: '/', //"root" directory
+			templateUrl: 'partials/home.html',
+			controller: 'HomepageCtrl'
+		})
+
+		.state('form', {
+			url: '/Form', 
+			templateUrl: 'partials/form.html',
+			controller: 'FormCtrl'
+		})
+})
+
+.controller('HomepageCtrl', ['$scope', '$http', function($scope, $http) {
+
+}])
